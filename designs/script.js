e6239db5e8b74e13b0130e0101d68f88e4a5f53b
Caroline Spooner <caroline.delourdes@gmail.com>
2015-05-11 20:59:53 -0700

    working prototype for friend select

---
+++ designs/script.js
@@ -0,0 +1,162 @@
+/* application init */
+$(document).ready(function(){
+	/* center introduction on load or resize */
+	var ActiveSectionId = "section#introduction"
+	WeMeet.center(ActiveSectionId,false)
+	$( window ).resize(function() {
+	  WeMeet.center(ActiveSectionId,false)
+	});
+	$("#facebook-login").click(function(){
+		$(this).attr("disabled", true)	
+		FB.login(function(response) {
+			WeMeetFacebook.statusChangeCallback(response)			
+		}, {scope: 'user_location,user_friends'});
+	})
+});
+/* library */
+var WeMeet = {
+	me: [],
+	friends: [],
+	numFriends: 0,
+	numPeople: 0,
+	numPeopleSelected: 0,
+	/* center active section */
+	center: function(ActiveSectionId,animate,reveal){
+		var introMargins = ($(window).height() - $(ActiveSectionId).height()) / 2
+		if(animate){
+			$(ActiveSectionId).show("slow").animate({ 
+			    'padding' : introMargins+"px 0px"
+			}, "slow");
+		}
+		else {
+			$(ActiveSectionId).removeAttr("hidden")
+			$(ActiveSectionId).css("padding",introMargins+"px 0px")
+		}
+	}, // center
+	/* select friends*/
+	initFriendSelect: function(){
+		$("#introduction").slideUp("slow")
+		$("#who").fadeIn("slow")
+		WeMeetFacebook.getMe()// calls makeMe
+	}, // initFriendSelect()
+	// CALLBACK put user into WeMeet friends variable
+	makeMe: function (response){
+		// add current user if all the required information is there
+		if(typeof response.name != "undefined" && typeof response.picture != "undefined" && typeof response.location != "undefined")
+		{
+			WeMeet.me = {
+				name: response.name + " (You)",
+				photo: response.picture.data.url,
+				location: response.location.name
+			};
+			var html = WeMeet.makeFriendHTML(WeMeet.me)
+			$(html).insertBefore("#friends-selected #done-holder").removeClass("unselected").addClass("selected")
+		}
+		WeMeetFacebook.getFriends()// calls makeFriendsList
+	},
+	// CALLBACK put user friends into WeMeet friends variable
+	makeFriendsList: function (response){
+		WeMeet.numFriends = response.data.length
+		for(var i = 0; i < WeMeet.numFriends; i++)
+		{	
+			// Add friend if all the required information is there.
+			if(typeof response.data[i].name != "undefined" && typeof response.data[i].picture != "undefined" && typeof response.data[i].location != "undefined")
+			{
+				WeMeet.friends.push({
+					name: response.data[i].name,
+					photo: response.data[i].picture.data.url,
+					location: response.data[i].location.name
+				});
+			}
+		}
+		// update counter in case some friends were skipped due to missing information
+		WeMeet.numFriends = WeMeet.friends.length
+		WeMeet.addFriendsHTML()
+		if(typeof WeMeet.me.name != "undefined")
+		{
+			WeMeet.numPeople = WeMeet.numFriends + 1
+			WeMeet.numPeopleSelected = 1
+		}
+		else
+			WeMeet.numPeople = WeMeet.numFriends
+		WeMeet.initFriendBehavior()
+	}, // makeFriendsList()
+	// add HTML for each friend onto the page
+	addFriendsHTML: function(){
+		var html = ""
+		for(var i = 0; i < WeMeet.numFriends; i++)
+		{
+			html += WeMeet.makeFriendHTML(WeMeet.friends[i])
+		}
+		$("#friends-select").append(html)
+	}, // drawFriends()
+	makeFriendHTML: function(friend)
+	{
+		var html = ""
+		html += '<div class="friend unselected small-6 medium-4 large-3 columns" data-equalizer-watch>'
+		html += '<div class="friend-wrapper clearfix">'
+		html += '<div class="profile-photo">'
+		html += '<div class="photo-wrap"><img src="'+ friend.photo +'" alt="'+ friend.name +' Profile Photo"/></div>'
+		html += '</div>' //.profile-photo
+		html += '<div class="details">'
+		html += '<div class="friend-name">'+ friend.name +'</div>'
+		html += '<div class="friend-location">' + friend.location+'</div>'
+		html += '</div>' //.details
+		html += '</div>' //.friend-wrapper
+		html += '</div>' //.friend
+		return html
+	},
+	// set up  appropriate action listeners - not to be called until after friends are drawn
+	initFriendBehavior: function(){
+			$(document).foundation({
+  				equalizer : {
+		    		equalize_on_stack: true
+		  		}
+			});
+			// event listener to move or remove friend from selection area
+			$(".friend").click(function() {
+				if($(this).hasClass("selected"))
+				{
+					// going from all to less than all
+					if(WeMeet.numPeopleSelected >= WeMeet.numPeople){
+						$("#all-going").fadeOut()
+					}
+					// going from 2 to less than 2
+					if(WeMeet.numPeopleSelected === 2){
+						$("#no-friends").fadeIn()
+						$("#done-selecting").fadeOut()
+					}
+					// remove 1 from current selection
+					WeMeet.numPeopleSelected--;
+					// update flag
+					$(this).removeClass("selected").addClass("unselected")
+					// move to selected
+					$(this).appendTo("#friends-select")
+					$(document).foundation('equalizer', 'reflow');
+				}
+				else
+				{
+					// going from 0 to 1 selections
+					if(WeMeet.numPeopleSelected === 0)
+					{
+						$("#all-going").fadeOut()
+					}
+					// going from 1 to 2 selections
+					else if (WeMeet.numPeopleSelected === 1){
+						$("#no-friends").fadeOut()
+						$("#done-selecting").fadeIn()
+					}
+					// add 1 to current selection
+					WeMeet.numPeopleSelected++;
+					// all available Facebook Friends have been selected (and there was at least one to pick)
+					if(WeMeet.numPeopleSelected >= WeMeet.numPeople && WeMeet.numPeople > 0)
+						$("#all-going").fadeIn()
+					// update flag
+					$(this).removeClass("unselected").addClass("selected")
+					// move to unselected
+					$(this).insertBefore("#friends-selected #done-holder")
+					$(document).foundation('equalizer', 'reflow');
+				}
+			}); // friend.click()
+	} // initFriendBehavior()
+}; // WeMeet
--- facebook.js
+++ facebook.js
@@ -1,39 +1,38 @@
 $(document).ready(function() {
-  $.getScript('//connect.facebook.net/en_UK/all.js', function(){
-  	FB.init({
-  		appId: '342664942589806',
-  	});     
-  	$('#loginbutton,#feedbutton').removeAttr('disabled');
-  	FB.getLoginStatus(WeMeetFacebook.statusChangeCallback);
-  })
+	$.getScript('//connect.facebook.net/en_UK/all.js', function(){
+		FB.init({
+			appId: '342664942589806',
+		});     
+		$('#loginbutton,#feedbutton').removeAttr('disabled');
+		FB.getLoginStatus(WeMeetFacebook.statusChangeCallback);
+	})
 });
 var WeMeetFacebook = {
 	statusChangeCallback: function(response){		
 		if (response.status === 'connected') {				
 			// Logged into your app and Facebook.
+			// To do.. check for Friends AND Location Permission
 			FB.api("/me/permissions/user_friends", function (response) {
 				// response returned
-	      		if (response && !response.error) {
+				if (response && !response.error) {
 	      			// has location permission
 	      			if(response.data[0] && response.data[0].status === "granted")
 	      			{
-	      				WeMeet.center("section#who",false)
-						WeMeet.fadeaway("section#introduction")
-						WeMeet.initFriendSelect()
+	      				WeMeet.initFriendSelect()
 	      			}
 	      			// does not have location permission
-					else
+	      			else
 	      			{      				
 	      				$("#facebook-login").removeAttr("disabled")
-						$("#facebook-login").text("Share Facebook Friends List")		
+	      				$("#facebook-login").text("Authorize WeMeet")		
 	      			}  	
-      			}   
+	      		}   
       			// display an error (this shouldn't happen...)
       			else
       			{
       				$("#facebook-login").text("Facebook Error")	
       			}      					
-   			 });		
+      		});		
 		}
 		// The person is logged into Facebook, but on the app
 		else if (response.status === 'not_authorized') {
@@ -47,7 +46,10 @@ var WeMeetFacebook = {
 			$("#facebook-login").text("Login to Facebook")
 		}
 	},
-	GetFriends: function(){
-		FB.api("/me/friends", WeMeet.makeFriendsList)
+	getMe: function(){
+		FB.api("/me?fields=name,location,picture", WeMeet.makeMe)
+	},
+	getFriends: function(){
+		FB.api("/me/friends?fields=name,location,picture", WeMeet.makeFriendsList)
 	}	
 }
